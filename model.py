4e049eced51ab9c0cfd480e46f23de26d0de7547
Harry Wang <hgw2101@gmail.com>
2017-03-26 13:59:30 -0500

    use generator for better memory management

--- model.py
+++ model.py
@@ -1,49 +1,62 @@
 import csv
 import cv2
 import numpy as np
+import sklearn
+import random
 
 def process_image(fileline):
   filepath = './training_data/IMG/' + fileline.split("/")[-1]
   image = cv2.imread(filepath)
   return image
 
-lines = []
+def generator(samples, batch_size=32):
+  num_samples = len(samples)
+  while 1: #loop forever so the generator never stops
+    random.shuffle(samples)
+    for offset in range(0, num_samples, batch_size):
+      batch_samples = samples[offset:offset+batch_size]
+
+      images = []
+      measurements = []
+
+      center_image = process_image(line[0])
+      left_image = process_image(line[1])
+      right_image = process_image(line[2])
+
+      center_measurement = float(line[3])
+      correction = 0.2
+      left_measurement = center_measurement + correction
+      right_measurement = center_measurement - correction
+
+      flipped_center_image = np.fliplr(center_image)
+      flipped_left_image = np.fliplr(left_image)
+      flipped_right_image = np.fliplr(right_image)
+
+      flipped_center_measurement = -center_measurement
+      flipped_left_measurement = -left_measurement
+      flipped_right_measurement = -right_measurement
+
+      images.extend([center_image, left_image, right_image, flipped_center_image, flipped_left_image, flipped_right_image])
+      measurements.extend([center_measurement, left_measurement, right_measurement, flipped_center_measurement, flipped_left_measurement, flipped_right_measurement])
+
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+    yield sklearn.utils.shuffle(X_train, y_train)
+
+samples = []
 with open('./training_data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-    lines.append(line)
+    samples.append(line)
     # print("this is line: ", line[0])
 
+# using generators means we have to manually split train/validation sets and shuffle
+# the data, since we can no longer use Keras's validation_split and shuffle methods
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-images = []
-measurements = []
-for line in lines:
-  # print("this is filename: ", filename)
-  center_image = process_image(line[0])
-  left_image = process_image(line[1])
-  right_image = process_image(line[2])
-
-  center_measurement = float(line[3])
-  correction = 0.2
-  left_measurement = center_measurement + correction
-  right_measurement = center_measurement - correction
-
-  flipped_center_image = np.fliplr(center_image)
-  flipped_left_image = np.fliplr(left_image)
-  flipped_right_image = np.fliplr(right_image)
-
-  flipped_center_measurement = -center_measurement
-  flipped_left_measurement = -left_measurement
-  flipped_right_measurement = -right_measurement
-
-  images.extend([center_image, left_image, right_image, flipped_center_image, flipped_left_image, flipped_right_image])
-  measurements.extend([center_measurement, left_measurement, right_measurement, flipped_center_measurement, flipped_left_measurement, flipped_right_measurement])
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-# print("this is X_train shape: ", X_train.shape)
-# print("this is y_train shape: ", y_train.shape)
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 # model training
 from keras.models import Sequential
@@ -51,13 +64,14 @@ from keras.layers import Flatten, Dense, Dropout, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-input_shape = (X_train.shape[1],X_train.shape[2],X_train.shape[3])
+row, col, ch = 160, 320, 3
 
 model = Sequential()
 
 # preprocessing, normalizing (/255) and mean centering (-.5)
-model.add(Cropping2D(cropping=((60,25), (0,0)), input_shape=input_shape))
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(row, col, ch), output_shape=(row, col, ch)))
+
+model.add(Cropping2D(cropping=((60,25), (0,0))))
 
 # 1st CNN layer
 nb_filters =24
@@ -94,6 +108,6 @@ model.add(Dense(10))
 model.add(Dense(1)) #single node representing steering angle, unlike classification, which has # of final nodes equal to number of classes
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, nb_epoch=5, validation_split=0.2, shuffle=True)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
 
-model.save('model.h5')
+model.save('model_100.h5')
